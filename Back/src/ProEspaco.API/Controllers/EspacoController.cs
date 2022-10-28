0da3cd37af75f2a09064cb4fc263a14a702d0d09
Henrique Figueiredo <henricklima94@gmail.com>
2022-10-28 15:38:30 -0300

    test: Teste para retorno de clientes - API

---
+++ Back/src/ProEspaco.API/Controllers/EspacoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEspaco.Business.Interfaces.Repository;
+using System.Threading.Tasks;
+
+namespace ProEspaco.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EspacoController : ControllerBase
+    {
+        private readonly IClienteRepository _clienteRepository;
+        public EspacoController(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        [HttpGet]
+        [Route("eventos")]
+        public async Task<IActionResult> GetAll()
+        {
+            var clientes = await _clienteRepository.ObterTodos();
+
+            return Ok(clientes);
+        }
+    }
+}
